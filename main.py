2c73fb1b30e98be1b9ee45b00dd009dd326a2635
Moritz Maier <moritzmaier34@gmail.com>
2024-08-22 21:14:57 +0200

    new enemy + Info Tab

--- main.py
+++ main.py
@@ -27,6 +27,7 @@ enemy_nums = 0
 
 enemies = [] # x und y werte werden gespeichert
 enemies_2 = []
+enemies_3 = []
 add_enemy = False
 
 
@@ -43,6 +44,9 @@ exploding = pygame.transform.scale(exploding,(50,30))
 exploding_green = pygame.image.load("assets/explosiongreen.png")
 exploding_green = pygame.transform.scale(exploding_green,(50,35))
 
+exploding_purple = pygame.image.load("assets/explosionpurple.png")
+exploding_purple = pygame.transform.scale(exploding_purple,(50,35))
+
 init_enemy_y = 80
 enemy_spacing_x = 80
 enemy_spacing_y = 50
@@ -58,9 +62,22 @@ row_2 = 0
 level = 0
 
 def spawn_enemy():
-     global enemy_nums,enemy_x,enemy_y,init_enemy_y,col,row,level,enemy_x_2,enemy_y_2
+     global enemy_nums
+     global enemy_x
+     global enemy_y
+     global init_enemy_y
+     global col
+     global row
+     global level
+     global enemy_x_2
+     global enemy_y_2
+     global enemy_x_3
+     global enemy_y_3
+
+
      #add_enemy = True
      level += 1
+     print(level)
      if level <= 3:
           enemy_nums = 5
      else:
@@ -74,17 +91,21 @@ def spawn_enemy():
           enemy_y = init_enemy_y + row * enemy_spacing_y #+ 100
 
           enemy_x_2 = col * enemy_spacing_x + 50
+          enemy_x_3 = col * enemy_spacing_x +50
           if enemy_nums == 10:
                enemy_y_2 = init_enemy_y + row * enemy_spacing_y + 100
+               enemy_y_3 = init_enemy_y + row * enemy_spacing_y + 200
           else:
                enemy_y_2 = init_enemy_y + row * enemy_spacing_y + 50
-               
+               enemy_y_3 = init_enemy_y + row * enemy_spacing_y + 100
+          
      
           enemies.append({"x": enemy_x, "y": enemy_y,"speed":1,})
           enemies_2.append({"x2":enemy_x_2,"y2": enemy_y_2,"speed": 1})
+          enemies_3.append({"x3":enemy_x_3,"y3": enemy_y_3,"speed":1})
 
      init_enemy_y += enemy_spacing_y
-     if init_enemy_y >= 280:
+     if init_enemy_y >= 180:
           print("Maximum y erreicht!")
           init_enemy_y = 100
           enemy_y = 0
@@ -111,7 +132,7 @@ background = pygame.image.load("assets/background.png")
 
 # enemy 1
 enemy = pygame.image.load("assets/invader02a.png")
-enemy= pygame.transform.scale(enemy,(50,35))
+#enemy= pygame.transform.scale(enemy,(50,35))
 sprites = [
             pygame.image.load("assets/invader02a.png"),
             pygame.image.load("assets/invader02b.png"),
@@ -123,7 +144,7 @@ current_sprite = sprites
 animation_speed = 0.015
 
 enemy_2 = pygame.image.load("assets/enemy3_1.png")
-enemy_2 = pygame.transform.scale(enemy_2,(50,35))
+#enemy_2 = pygame.transform.scale(enemy_2,(50,35))
 sprites_2 = [
      pygame.image.load("assets/enemy3_1.png"),
      pygame.image.load("assets/enemy3_2.png")
@@ -133,12 +154,25 @@ index_2 = 0
 current_time_2 = 0
 current_sprite_2 = sprites_2
 
+enemy_3 = pygame.image.load("assets/enemy1_1.png")
+#enemy_3 = pygame.transform.scale(enemy_3,(50,35))
+sprites_3 = [
+     pygame.image.load("assets/enemy1_1.png"),
+     pygame.image.load("assets/enemy1_2.png")
+]
+
+index_3 = 0
+current_time_3 = 0
+current_sprite_3 = sprites_3
+
 enemy_rect_2 = enemy_2.get_rect()
 enemy_rect_2.topleft = (enemy_x_2,enemy_y_2)
 
 enemy_rect = enemy.get_rect()
 enemy_rect.topleft = (enemy_x,enemy_y)
 
+enemy_rect_3 = enemy_3.get_rect()
+enemy_rect_3.topleft = (enemy_x_3,enemy_y_3)
 
 # bullet
 bullet_size = 5
@@ -154,6 +188,7 @@ last_shot_time_2 = 0
 explosion_duration = 10
 explosions = []
 explosions_green = []
+explosions_purple = []
 # Text
 pygame.font.init()
 font = pygame.font.SysFont("Minecraft",55)
@@ -166,15 +201,73 @@ score_points = 0
 
 #level
 
+def show_info():
+     running = True
+     while running:
+          for event in pygame.event.get():
+               if event.type == pygame.QUIT:
+                    running = False
+               if event.type == pygame.KEYDOWN:
+                    running = False
+               if event.type == pygame.MOUSEBUTTONDOWN:
+                    if back_button_box.collidepoint(event.pos):
+                         running = False
+
+          mouse = pygame.mouse.get_pos()
+          grey = (32,32,32)
+          
+
+          screen.fill(grey)
+          score_text = font_big.render(f"SCORE {score_points}",True,white)
+          level_text = font_big.render(f"LEVEL {level}",True,white)
+          fps_text = font_big.render(f"FPS {FPS}",True,white)
+          player_live_text = font_big.render(f"LIVES {player_live}",True,white)
+          text_box = pygame.draw.rect(screen,"dark grey",[176,70,440,450])
+          alien_1_points = pygame.image.load("assets/=100.png")
+          alien_2_points = pygame.image.load("assets/=150.png")
+
+          a1 = 100
+          a2 = 150
+
+
+          text = font_small.render(
+               "White Aliens = 100\n"
+               "Green Aliens = 150\n"
+               "Purple Aliens = 200\n"
+               "Heart Item = 1 Live",False,"white")
+
+          alien_1_points = pygame.transform.scale(alien_1_points,(150,50))
+          alien_2_points = pygame.transform.scale(alien_2_points,(150,50))
+
+          #screen.blit(score_text,dest=(340,140))
+          #screen.blit(player_live_text,dest=(340,210))
+          #screen.blit(level_text,dest=(340,280))
+          #screen.blit(fps_text,dest=(340,350))
+          
+          #screen.blit(alien_1_points,dest=(0,150))
+          #screen.blit(alien_2_points,dest=(0,250))
+
+          
+          back_button_box = pygame.draw.rect(screen,grey,[10,540,200,200])
+
+          if back_button_box.collidepoint(mouse):
+               back_button = font.render("BACK",True,"red")
+          else:
+               back_button = font.render("BACK",True,white)
+
+
+          screen.blit(back_button,dest=(10,540))
+          screen.blit(text,dest=(200,100))
+          pygame.display.update()
+          clock.tick(60)
+
+
 enemy_bullets = []
 def show_menu():
      global player_live,level,score_points
-     teleport = False
      menu = True
      while menu:
           player_live = 4
-          level = 0
-          score_points = 0
           for event in pygame.event.get():
                if event.type == pygame.QUIT:
                     menu = False
@@ -187,6 +280,8 @@ def show_menu():
                          menu = False
                     if button_2.collidepoint(event.pos):
                          sys.exit("bye")
+                    if button_3.collidepoint(event.pos):
+                         show_info()
                     
           grey = (32,32,32)
           home = pygame.image.load("assets/background.png")
@@ -199,7 +294,7 @@ def show_menu():
 
           button_1 = pygame.draw.rect(screen,grey,[100,300,200,80])
           button_2 = pygame.draw.rect(screen,grey,[exit_pos_x,exit_pos_y,200,80])
-
+          button_3 = pygame.draw.rect(screen,grey,[exit_pos_x - 240,exit_pos_y,200,80])
           
           
 
@@ -221,6 +316,14 @@ def show_menu():
                 screen.blit(text_2,dest=(exit_pos_x,exit_pos_y))
                 #teleport = False
 
+          if button_3.collidepoint(mouse):
+               text_3 = font.render("INFO",True,"green")
+               screen.blit(text_3,dest=(exit_pos_x - 200,exit_pos_y))
+                #
+          else:
+               text_3 = font.render("INFO",True,"white")
+               screen.blit(text_3,dest=(exit_pos_x - 200,exit_pos_y))
+                #
 
           
           
@@ -238,7 +341,10 @@ def show_menu():
           #screen.blit(enemy_img,dest=(240,150))
           pygame.display.update()
           clock.tick(60)
-show_menu()
+#show_menu()
+
+
+
 
 def load_highscore():
      if os.path.exists("score.txt"):
@@ -261,6 +367,16 @@ def update_score(highscore_list):
      highscore_list.append(score_points)
      return highscore_list
 
+def remove_duped():
+     with open("score.txt","r") as file:
+          lines = file.readlines()
+
+          unique_lines = list(set(lines))
+
+          with open("score.txt","w") as file:
+               for line in unique_lines:
+                    file.write(line)
+remove_duped()
 
 class Protection:
      def __init__(self,x,y):
@@ -357,12 +473,14 @@ def decrease_player_life(amount):
      if player_live < 0:
           player_live = 0
 
-highscores = load_highscore()
 
 prot = Protection(100,450)
 item = Item(random.randint(0,500),random.randint(0,100))
-highscores = update_score(highscores)
-save_score(highscores)
+
+highscores = load_highscore()
+
+print(f"Current Score: {score_points}")
+print(f"Current Highscore: {highscores}")
 
 run = True
 while run:
@@ -384,6 +502,7 @@ while run:
                          
      
 
+    
                     
      # update position
      player_rect.topleft = (player_x,player_y)
@@ -392,14 +511,31 @@ while run:
      # enemy bullets
      
      for i3 in enemies:
-          enemy_rect.topleft = (i3["x"] ,i3["y"])
+          enemy_rect.topleft = (i3["x"] ,i3["y"]) 
           if random.randint(1,600) < 2:
                for i in range(1):
+                    print("Enemy_1: Shot a bullet!")
                     enemy_bullet_pos = [i3["x"] + 25,i3["y"] + 50]
                     enemy_bullets.append(enemy_bullet_pos)
                     shoot_sound.play() 
      
-     
+     for x in enemies_2:
+          enemy_rect_2.topleft = (x["x2"],x["x2"])
+          if random.randint(1,700) < 2:
+               for i in range(1):
+                    print("Enemy_2: Shot a bullet!")
+                    enemy_bullet_pos = [x["x2"]+25,x["y2"]+50]
+                    enemy_bullets.append(enemy_bullet_pos)
+                    shoot_sound.play()
+
+     for y in enemies_3:
+          enemy_rect_3.topleft = (y["x3"],y["x3"])
+          if random.randint(1,800) < 2:
+               for i in range(1):
+                    print("Enemy_3: Shot a bullet!")
+                    enemy_bullet_pos = [y["x3"]+25,y["y3"]+20]
+                    enemy_bullets.append(enemy_bullet_pos)
+                    shoot_sound.play() 
      
      
 
@@ -420,13 +556,35 @@ while run:
 
 
           screen.blit(current_sprite_2,(enemy_2["x2"],enemy_2["y2"]))
-          enemy_2["x2"] += enemy_2["speed"] * enemy_direction
+
+          
+          enemy_2["x2"] += enemy_2["speed"] * enemy_direction 
+          
+          
           
 
           if enemy_2["x2"] <= 0 or enemy_2["x2"] >= screen_width - enemy.get_width():
                     enemy_direction *= -1
                     break
-    
+     
+     for enemy_3 in enemies_3:
+          index_3 += 0.01
+          if index_3 >= len(sprites_3):
+               index_3 = 0
+
+          current_sprite_3 = sprites_3[int(index_3)]
+          current_sprite_3 = pygame.transform.scale(current_sprite_3,(50,35))
+          screen.blit(current_sprite_3,(enemy_3["x3"],enemy_3["y3"]))
+
+          
+          enemy_3["x3"] += enemy_3["speed"]  * enemy_direction
+         
+
+          if enemy_3["x3"] <= 0 or enemy_3["x3"] >= screen_width - enemy.get_width() - 50:
+               enemy_direction *= -1
+               break
+
+
      for i3 in enemies:
           index += 0.01
          
@@ -446,7 +604,9 @@ while run:
           i3["x"] += i3["speed"] * enemy_direction
           
 
-          if i3["x"] <= 0 or i3["x"] >= screen_width - enemy.get_width():
+          
+
+          if i3["x"] <= 0 or i3["x"] >= screen_width - enemy.get_width() - 50:
                     enemy_direction *= -1
                     break
          
@@ -597,9 +757,9 @@ while run:
                
                     
      if player_live == 0:
-               show_menu() 
+          # game over sound?
+          show_menu() 
                
-
 
      # Kollision
      if player_x < 0:
@@ -608,9 +768,9 @@ while run:
           player_x = 750
      
 
-     if len(enemies) == 0 and len(enemies_2) == 0:
+     if len(enemies) == 0 and len(enemies_2) == 0 and len(enemies_3) == 0:
           add_enemy = True
-          enemy_nums += 10
+          #enemy_nums += 5
           if add_enemy:
                spawn_enemy()    
 
@@ -660,7 +820,22 @@ while run:
                          enemies_2.remove(n)
                          explosions_green.append({"pos2": (n["x2"], n["y2"]), "timer2": explosion_duration})
                          break
-                   
+          for y in enemies_3:
+               enemy_rect_3 = pygame.Rect(y["x3"],y["y3"],50,35)
+
+               for bullet in bullets:
+                    bullet_rect = pygame.Rect(bullet[0], bullet[1], 9, 20)
+                    if bullet_rect.colliderect(enemy_rect_3):
+                         hit_sound.play()
+                         score_points += 200  # Punkte für enemy_2
+                         enemy_nums -= 1
+                         bullets.remove(bullet)
+                         enemies_3.remove(y)
+                         explosions_purple.append({"pos3": (y["x3"], y["y3"]), "timer3": explosion_duration})
+                         break
+
+
+
      for explosion in explosions:
           screen.blit(exploding,dest=(explosion["pos"]))
           explosion["timer"] -= 1
@@ -675,13 +850,18 @@ while run:
           if explosion2["timer2"] <= 0:
                explosions_green.remove(explosion2)
 
-     
-     
-           
+     for explosion3 in explosions_purple:
+          screen.blit(exploding_purple,dest=(explosion3["pos3"]))
+          explosion3["timer3"] -= 1
+
+          if explosion3["timer3"] <= 0:
+               explosions_purple.remove(explosion3)
+        
      
 
      score_text = font_small.render(f"SCORE {score_points}",True,white)
-     highscore_text = font_small.render(f"HighScore {highscores[0] if highscores else 0}",False,white)
+     #highscore_text = font_small.render(f"HSCORE",False,white)# {highscores[0] if highscores else 0}
+     highscore_text_2 = font_small.render(f"{highscores[0] if highscores else 0}",False,white)
      level_text = font_small.render(f"LEVEL {level}",True,white)
      fps_text = font.render(f"FPS {FPS}",True,white)
      player_live_text = font_small.render(f"LIVES {player_live}",True,white)
@@ -690,8 +870,9 @@ while run:
 
      screen.blit(score_text,dest=(10,10))
      screen.blit(player_live_text,dest=(650,10))
-     screen.blit(level_text,dest=(10,50))
-     screen.blit(highscore_text,dest=(270,10))
+     #screen.blit(level_text,dest=(10,50))
+     #screen.blit(highscore_text,dest=(10,50))
+     screen.blit(highscore_text_2,dest=(340,10))
      #screen.blit(fps_text,dest=(10,100))
 
      if __name__ == "__main__":
@@ -699,6 +880,8 @@ while run:
           bullet_func()
           input_func()
           
+          highscores = update_score(highscores)
+          save_score(highscores)
           prot.update()
           item.update()
      pygame.display.update()
